92a8d187e0e97800f027ef58fc959e905a6ba9e1
Lucas Vieira <lucas.vieira@chefsclub.com.br>
2017-10-23 20:15:17 -0200

    Implemented first version of the second stage menu

--- main.pl
+++ main.pl
@@ -2,6 +2,8 @@ use strict;
 use warnings;
 use Switch;
 
+use Term::ANSIScreen qw(cls);
+
 sub main {
   print "*************************************************************************\n";
   print "\t\tTrabalho de Linguagens de Programacao - Parte II\n";
@@ -12,37 +14,44 @@ sub main {
   choose_currency_type();
 }
 
-sub fisical_currencies_menu {
-
+sub clear_screen {
+  my $clear_screen = cls();
+  print $clear_screen;
+}
+sub fisical_currencies_menu  {
+  clear_screen();
+  print "\t\t\tMenu - Fisical Currencies\n\n";
+  exit();
 }
 
 sub crypto_currencies_menu {
-
+  clear_screen();
+  print "\t\t\tMenu - Crypto Currencies\n\n";
+  exit();
 }
 
 sub goodbye_message {
-
+  clear_screen();
+  print "\t\t\tThanks for using our program!\n\n";
+  exit();
 }
 
 sub choose_currency_type {
   print "Choose the currency type:\n\n";
 
   print "\t1 - Fisical Currencies\n";
-  print "\t2 - Crypto Currencies\n";
-  print "\t3 - Exit\n";
+  print "\t2 - Crypto Currencies\n\n";
+  print "\tPress anything else to exit.\n\n";
+  print "Option: ";
 
   my $choice = <STDIN>;
 
-  switch($choice) {
-    case 1 {
-      fisical_currencies_menu();
-    }
-    case 2 {
-      crypto_currencies_menu();
-    }
-    else {
-      goodbye_message();
-    }
+  if ($choice == 1) {
+    fisical_currencies_menu();
+  } elsif ($choice == 2) {
+    crypto_currencies_menu();
+  } else {
+    goodbye_message();
   }
 }
 
